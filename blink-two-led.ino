1f28c15fd9494e52e49c72b414658dac67165acb
naufalalmajid <70688082+naufalalmajid@users.noreply.github.com>
2023-07-28 21:18:04 +0700

    Update blink-two-led.ino

--- blink-two-led.ino
+++ blink-two-led.ino
@@ -4,18 +4,18 @@
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(Led_Green, OUTPUT); //state output for green red color
-  pinMode(Led_Blue, OUTPUT); //state output for blu red color
+  pinMode(Led_Green, OUTPUT); //state output for green color
+  pinMode(Led_Blue, OUTPUT); //state output for blue color
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(Led_Green, HIGH); // state off green
+  digitalWrite(Led_Green, HIGH); // state on green
   delay(500); //time (ms) on 
   digitalWrite(Led_Green, LOW); // state off green
   delay(500); //time (ms) off 
-  digitalWrite(Led_Blue, HIGH); // state off green
+  digitalWrite(Led_Blue, HIGH); // state on blue
   delay(500); //time (ms) on 
-  digitalWrite(Led_Blue, LOW); // state off green
+  digitalWrite(Led_Blue, LOW); // state off blue
   delay(500); //time (ms) off 
 }
